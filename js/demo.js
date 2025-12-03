ed80ebbfda33da769d154505cc408a2bc8ddcfdc
Trung Bui <buitrung3005@gmail.com>
2025-12-03 14:17:26 +0700

    bai_ngay_3/12

--- js/demo.js
+++ js/demo.js
@@ -22,12 +22,31 @@
 // document.getElementById("btnTest").onclick = function(){
 //     alert("Nhìn cái chos gì :3");
 // };
+//----------------------------
+// document.getElementById("myForm")
+//     .addEventListener("submit", function(event) {
+//     let name = document.getElementById("name").value;
+//     if(name ===""){
+//         alert("vui lòng nhập tên");
+//         event.preventDefault();
+//     }
+// });
+//--------------------------------
+const form = document.getElementById("formDK");
+const fullname = document.getElementById("fullname");
+const address = document.getElementById("address");
+const gender = document.getElementById("gender");
+const email = document.getElementById("email");
+const note = document.getElementById("note");
 
-document.getElementById("myForm")
-    .addEventListener("submit", function(event) {
-    let name = document.getElementById("name").value;
-    if(name ===""){
-        alert("vui lòng nhập tên");
-        event.preventDefault();
-    }
-});
+form.addEventListener("submit", function(e) {
+    e.preventDefault();
+
+        alert(
+            "Họ tên: " + fullname.value +
+            "\nĐịa chỉ: " + address.value +
+            "\nGiới tính: " + gender.value +
+            "\nEmail: " + email.value +
+            "\nGhi chú: " + note.value
+        );
+    });
